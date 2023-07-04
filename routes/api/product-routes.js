d2c3a0039c002e6ec50593a4f7cbf12476382b8b
Arbitrary Stag <will.weaver.iv@gmail.com>
2023-07-03 21:11:27 -0700

    made some adjustments to the update api route for products.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -77,14 +77,14 @@ router.put('/:id', (req, res) => {
     },
   })
     .then((product) => {
+      if (req.body.tagIds && req.body.tagIds.length) {
 
         ProductTag.findAll({
           where: { product_id: req.params.id }
         }).then((productTags) => {
           // create filtered list of new tag_ids
           const productTagIds = productTags.map(({ tag_id }) => tag_id);
-          const split_string = req.body.tagIds.replace(/\s/g, '').split(",");
-          const newProductTags = split_string
+          const newProductTags = req.body.tagIds
             .filter((tag_id) => !productTagIds.includes(tag_id))
             .map((tag_id) => {
               return {
@@ -95,21 +95,23 @@ router.put('/:id', (req, res) => {
 
           // figure out which ones to remove
           const productTagsToRemove = productTags
-            .filter(({ tag_id }) => !split_string.includes(tag_id))
+            .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
             .map(({ id }) => id);
           // run both actions
           return Promise.all([
             ProductTag.destroy({ where: { id: productTagsToRemove } }),
             ProductTag.bulkCreate(newProductTags),
           ]);
-        })
-        .then((updatedProductTags) => res.json(updatedProductTags))
-        .catch((err) => {
-          // console.log(err);
-          res.status(400).json(err);
-        }); 
-    })  
-  });
+        });
+      }
+
+      return res.json(product);
+    })
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
+});
 
 // delete one product by its `id` value
 router.delete('/:id', async (req, res) => {
